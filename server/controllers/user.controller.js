ff73369b03aabf0f573a76de5de315b56c6e4c06
Lucky <lk899202@gmail.com>
2024-12-14 14:25:40 +0530

    update .env file

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import razorpay from "razorpay";
 
 //register user
 const registerUser = async (req, res) => {
@@ -67,6 +68,50 @@ const userCredits = async (req, res) => {
     res.json({ success: true, credits: user.creditBalance, user: user.name });
   } catch (error) {
     console.error(`Error while user credits: ${error.message}`);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { userId, planId } = req.body;
+
+    const userData = await User.findById(userId);
+    if (!userData || !planId) {
+      return res.json({ success: false, message: "Missing details" });
+    }
+
+    let credits, plan, amount, date;
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+
+      case "Advanced":
+        plan = "Advanced";
+        credits = 500;
+        amount = 50;
+        break;
+
+      case "Business":
+        plan = "Business";
+        credits = 5000;
+        amount = 250;
+        break;
+
+      default:
+        return res.json({success: false, })
+        break;
+    }
+  } catch (error) {
+    console.log(`error while payment: ${error}`);
     res.json({ success: false, message: error.message });
   }
 };
