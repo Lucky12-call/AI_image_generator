ac82101879880d008e54dfea1feb43df1e6e1e93
Lucky <lk899202@gmail.com>
2025-01-29 17:20:21 +0530

    add frontend url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "*",
+    origin: "https://ai-image-generator-k5a9.vercel.app",
     methods: ["GET", "POST", "UPDATE", "DELETE"],
     credentials: true,
 }));
